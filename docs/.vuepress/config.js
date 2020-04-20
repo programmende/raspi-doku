713d2fc3639e707586e6ef98eb6f6b2890de9991
Christian Zörkler <christian.zoerkler@programmende.de>
2020-04-20 17:54:02 +0200

    some plugin and config fixes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,59 +1,61 @@
 module.exports = {
-    // base: "/raspi-doku/",
-    plugins: [
+  //base: "/docs/",
+  plugins: [
     ['@vuepress/back-to-top', true],
     '@vuepress/last-updated',
     '@vuepress/nprogress',
     '@vuepress/medium-zoom'
-      ],
-      markdown: {
-        lineNumbers: false,  
-     },
-    host: 'localhost',
-    title: 'Raspberry Pi Homeserver',
-    description: 'erklärt am Praxisbeispiel',
-    themeConfig: {
-        navbar: true,
-      smoothScroll: false,
-      docsRepo: 'programmende/raspi-doku/docs',
-      editLinks: true,
-      editLinkText: 'Seite bearbeiten',
-        nav: [
-            {
-                text: 'Guide',
-                link: '/guide/'
-          }, {
-            text: 'Docker Cheatsheet',
-            link: '/guide/docker-cheatsheet.html'
+  ],
+  markdown: {
+    lineNumbers: false,
+  },
+  host: 'localhost',
+  title: 'Raspberry Pi Homeserver',
+  description: 'erklärt am Praxisbeispiel',
+  themeConfig: {
+    navbar: true,
+    smoothScroll: false,
+    repo: 'programmende/raspi-doku',
+    docsDir: 'docs',
+    editLinks: true,
+    //docsDir: '',
+    editLinkText: 'Seite bearbeiten',
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/'
+      }, {
+        text: 'Docker Cheatsheet',
+        link: '/guide/docker-cheatsheet.html'
       },
-          
-        ],
 
-        sidebar: {
-            '/guide/': getGuideSidebar('Guide', 'Extras'),
-        }
+    ],
+
+    sidebar: {
+      '/guide/': getGuideSidebar('Guide', 'Extras'),
     }
+  }
 }
 
-function getGuideSidebar (groupA, groupB) {
-    return [
-      {
-        title: groupA,
-            collapsable: false,
-        children: [
-            '',
-            'raspbian',
-            'openmediafault'
+function getGuideSidebar(groupA, groupB) {
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      children: [
+        '',
+        'raspbian',
+        'openmediafault'
 
-        ]
-      },
-      {
-        title: groupB,
-        collapsable: true,
-        children: [
-          'quick-guide',
-          'docker-cheatsheet',
-        ]
-      }
-    ]
-  }
+      ]
+    },
+    {
+      title: groupB,
+      collapsable: true,
+      children: [
+        'quick-guide',
+        'docker-cheatsheet',
+      ]
+    }
+  ]
+}

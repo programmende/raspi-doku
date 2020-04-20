e29ab420db91f7643a589d264b50fdb270e20ed0
Christian Zörkler <christian.zoerkler@programmende.de>
2020-04-20 19:38:04 +0200

    add pwa support

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,38 @@ module.exports = {
     ['@vuepress/back-to-top', true],
     '@vuepress/last-updated',
     '@vuepress/nprogress',
-    '@vuepress/medium-zoom'
+    '@vuepress/medium-zoom',
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ]
   ],
+
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#42b983' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#42b983' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+
   markdown: {
     lineNumbers: false,
   },
+  
   host: 'localhost',
+  
   title: 'Raspberry Pi Homeserver',
+  
   description: 'erklärt am Praxisbeispiel',
+  
   themeConfig: {
     navbar: true,
     smoothScroll: false,
